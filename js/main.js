ee1e7f7bec47bafb31c52ef5b431c47b493ea2af
Alo Ndubuisi <alo.ndubuisi07@gmail.com>
2025-09-01 19:28:42 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function sayHello() {
+    alert("Hello! Thanks for visiting my portfolio 😊");
+}
+
+function toggleMenu() {
+    const menu = document.getElementById("mobileMenu");
+    menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+}
